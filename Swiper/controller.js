129af0275d7c548583d68502a3a0fc0ddcf32e14
steven cardona <you@example.com>
2022-01-09 14:59:21 -0500

    V-2 controller swiper

--- Swiper/controller.js
+++ Swiper/controller.js
@@ -1,17 +1,16 @@
 const swiper = new Swiper(".swiper", {
-  // Optional parameters
-  direction: "horizontal",
-  spaceBetween: 30,
-  effect: "fade",
-  loop: true,
-  // If we need pagination
-  pagination: {
-    el: ".swiper-pagination",
-  },
+	// Optional parameters
+	loop: true,
 
-  // Navigation arrows
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
+	// If we need pagination
+	pagination: {
+		el: ".swiper-pagination",
+		dynamicBullets: true,
+	},
+
+	// Navigation arrows
+	navigation: {
+		nextEl: ".swiper-button-next",
+		prevEl: ".swiper-button-prev",
+	},
 });
